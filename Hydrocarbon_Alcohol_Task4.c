854f87d9e4b8a5276ebdfb40929d851f839c198b
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2025-11-19 20:06:33 +0600

    Update and rename Task4.c to Hydrocarbon_Alcohol_Task4.c

---
+++ Hydrocarbon_Alcohol_Task4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+     int n;
+     printf("Enter the value of n = ");
+     scanf("%d",&n);
+     printf("C%dH%dOH",n,2*n+1);
+    return 0;
+}
+
+/*In chemistry, alcohol is an organic compound that carries at least
+one hydroxyl functional group (-OH) bound to a saturated carbon
+atom. An important class of alcohols, of which methanol and
+ethanol are the simplest members, includes all compounds for
+which the general formula is
+C n H 2n+1 OH.
+
+Write a C program that would take the value of n as input and
+print the corresponding simple alcohol formula.*/
--- Task4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-     int n;
-     printf("Enter the value of n = ");
-     scanf("%d",&n);
-     printf("C%dH%dOH",n,2*n+1);
-    return 0;
-}
