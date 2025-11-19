861126bccd325c002179e4a337bacff19e4dd3f6
Jamil Hasan Riju <jamilhasan@iut-dhaka.edu>
2025-11-19 20:04:39 +0600

    Update and rename Task2.c to Conversion_Task2.c

---
+++ Conversion_Task2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void) {
+ 
+    double 𝜋=3.141593;
+    double rad;
+ rad= 45.02*𝜋/180.0;
+printf("The Radian is = %.4lf",rad);
+return 0;
+
+}
+
+
+/*Two most common units for measuring angles are degrees(°) and
+radians(c). The relationship between them is as follows.
+Write a C program that would convert 45.02° to radians. Consider
+the value of π as 3.141593.*/
--- Task2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(void) {
- 
-    double 𝜋=3.141593;
-    double rad;
- rad= 45.02*𝜋/180.0;
-printf("The Radian is = %.4lf",rad);
-return 0;
-}
